9b9a1f3abb1d0623b1c785d7f5c50e204d23659a
Patrick Moreira <patrickmoreira1995@gmail.com>
2018-08-17 16:52:17 -0300

    Implementation

--- Lagrange.cpp
+++ Lagrange.cpp
@@ -1,14 +1,14 @@
 /*
-	@title: Interpolação Polinomial: Método de Lagrange (Utilizando um polinomio interpolador de grau 2 e outro de grau 3) 
- 	@author: Patrick Moreira 
- 	@date: 01/07/2018
+	@ title: Polynomial interpolation: Lagrange method (using a degree 2 and degree 3 interpolation polynomial)
+	@author: Patrick Moreira
+	@date: 2018/07/01
 
 	-----------------------------------------------------------------
-	-> O Polinômio Interpolador de Lagrange é dado pela seguinte fórmula: 
+	->The Lagrange Interpolator Polynomial is given by the following formula: 
 	L(x) = y0.L0(x) + y1.L1(x) + ... + yn.Ln(x)
 	___________________________________________
 	
-	-> Determinar Lk(X):
+	-> To determine Lk(X):
 	L0(X) = [(x-x1).(x-x2)] / [(x0-x1).(x0-x2)]			
 				
 	L1(X) = [(x-x0).(x-x2)] / [(x1-x0).(x1-x2)]			
@@ -20,10 +20,10 @@
 
 ------------------------------------------------------------------ */
 
-/* Comentarios Gerais:
- * Na primeira linha do arquivo "points.txt" e possivel escolher qual o grau polinomio interpolador desejado. 
- * Por exemplo: Se desejo um polinomio interpolador de grau 2 devo escolher 3 pontos, portanto, o parametro no arquivo deve ser igual a 3. 	
- */
+ /* General Comments:
+  * In the first line of the file "points.txt" it is possible to choose which degree the interpolator polynomial you want.
+  * For example: If I want an interpolator polynomial of degree 2 I should choose 3 points, so the parameter in the file must be equal to 3.
+  */
 
 
 #include <iostream>
@@ -37,24 +37,24 @@ typedef struct{
 	double y;
 }Points; 
 
-#define DISTANCE 1.9 // Distancia media de seguranca minima entre as bicicletas e os carros. 
+#define DISTANCE 1.9 // Minimum average safety distance between bicycles and cars. 
 
-Points* readFilePoints (int&); // Leitura do arquivo que contém os pontos a serem utilizados no cálculo dos polinomios interpoladores. 
+Points* readFilePoints (int&);
 double lagrange (Points*, int);
 void printPoints (Points*, int);  
 
 int main (){
 	Points* points;
-	int degree; // Grau do polinomio a ser utilizado posteriormente.	
+	int degree; 	
 	double pn;
 
 	points = readFilePoints (degree);
 	printPoints (points, degree);
 	
-	//cout << "CALCULO DE Li(X):" << endl; // Pode-se descomentar a 'linha 99' para visualizar os calculos de Li(X), mas nao e necessario. 
+	//cout << "CALCULO DE Li(X):" << endl; // Delete the comment from 'line 99' to view the Li (X) calculations, but this is not necessary. 
 	pn = lagrange (points, degree);
 	cout << endl;
-	cout << "O Polinomio Interpolador de Lagrange de grau " << degree-1 << " e aproximadamente " << fixed << setprecision(4) << pn << endl;	
+	cout << "The Lagrange interpolator polynomial of degree " << degree-1 << " is approximately " << fixed << setprecision(4) << pn << endl;	
 
 	return 0;
 }
@@ -72,7 +72,7 @@ Points* readFilePoints (int& n){
 		}
 		file.close();
 	}else{
-		cout << "Nao foi possivel abrir o arquivo." << endl;	
+		cout << "Can not open file." << endl;	
 	}
 	return points;
 }
@@ -81,30 +81,30 @@ double lagrange (Points* points, int n){
 	double prod_numerator [n]; 
 	double prod_denominator [n];
 	double lk [n];
-	double lip = 0.0; // 'lip' = polinomio interpolador de lagrange.
+	double lip = 0.0; 
 
-	for (int i = 0; i < n; i++){ // Inicializa-se com '1' os vetores que serao utilizados no produtorio e com '0' no somatorio, nos calculos dos Lk(X) e do 'lip' respectivamente.
+	for (int i = 0; i < n; i++){ // They initialize with the '1' the vectors that will be used in the production, and with '0' those that will be used sum, in the calculations of Lk (X) and 'lip' respectively.		
 		prod_numerator[i] = 1;
 		prod_denominator[i] = 1;
 		lk[i] = 0;
 	}
 	for (int i = 0; i < n; i++){
 		for (int j = 0; j < n; j++){
-			if (j != i){ // Dessa maneira nunca ocorrera uma divisao invalida (por zero).
+			if (j != i){ // Do not divide by zero
 				prod_numerator[i] *= (DISTANCE-points[j].x); 
 				prod_denominator[i] *= (points[i].x-points[j].x); 
 			}	
 		}	
-		lk[i] = prod_numerator[i]/prod_denominator[i]; // Formula para calcular Lk(X).
-		//cout << "L" << i << "(X) = " << fixed << setprecision(4) << lk[i] << endl; // 'linha 99' referenciada na linha '54'.
-		lip += (points[i].y*lk[i]); // Formula do Polinomio Interpolador de Lagrange.
+		lk[i] = prod_numerator[i]/prod_denominator[i]; // Formula to calculate Lk(X).
+		//cout << "L" << i << "(X) = " << fixed << setprecision(4) << lk[i] << endl; // 'linha 99' referenced on line '54'.
+		lip += (points[i].y*lk[i]); // Lagrange Interpolator Polynomial Formula.
 	}
 	return lip;
 }
 
 void printPoints (Points* points, int n){
 	cout << endl;
-	cout << "PONTOS ESCOLHIDOS:" << endl;
+	cout << "USED POINTS:" << endl;
 	cout << " x   y" << endl;
 	for (int i = 0; i < n; i++){
 		cout << fixed << setprecision(1) << points[i].x << " " << points[i].y << endl;
